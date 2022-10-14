e6358e1d1d53e94fa8dbf47e1b1579386370965f
Chris Philbin <chrisp@runway.com.au>
2022-10-14 06:32:05 -0400

    Adding cart dropdown functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,15 @@
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../contexts/cart.context";
 import "./cart-icon.styles.scss";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+
   return (
     <div className="cart-icon-container" onClick={() => setIsCartOpen(!isCartOpen)}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
